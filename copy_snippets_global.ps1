75b340e51128efcf3a9f9715ef51de53f89ef8e6
user.name: "Steve Tuccio <user.email: "steven.v.tuccio@gmail.com>
2022-03-05 14:38:44 -0500

    Added ability to install Snippet helper if wanted

--- copy_snippets_global.ps1
+++ copy_snippets_global.ps1
@@ -1,4 +1,6 @@
-
+Param (
+[switch] $Include_Helpers
+)
 
 $CurrentUser = (Get-ChildItem Env:\USERNAME).Value
 $SnippetHelper = "snippet_helper.code-snippets"
@@ -10,10 +12,17 @@ $Snippets = Get-ChildItem -Path .\ | Where-Object {$_.Name -like '*.code-snippet
 
 foreach ($Snippet in $Snippets)
 {
+
     if ($Snippet.Name -ne $SnippetHelper)
     {
         # Copy Each file to C:\Users\CurrentUser\AppData\Roaming\Code\User\snippets with overwrite
         Copy-Item -Path $Snippet.Name -Destination $SnippetDestPath
     }
+
+    if (($Include_Helpers -eq $True) -And ($Snippet.Name -eq $SnippetHelper))
+    {
+        Copy-Item -Path $Snippet.Name -Destination $SnippetDestPath
+    }
 }
 Write-Output "Install Complete."
+
--- copy_snippets_global.sh
+++ copy_snippets_global.sh
@@ -9,7 +9,7 @@ for snippet_filename in ./*.code-snippets
 do
 
     # # Don't copy Snippet Helper, that's a development tool
-    if [ "${snippet_filename:2}" != "$SNIPPET_HELPER_NAME" ]
+    if [ "${snippet_filename:2}" != "$SNIPPET_HELPER_NAME"  ]
     then
         cp ${snippet_filename} $SNIPPET_HOME/${snippet_filename:2}
         echo "     " ${snippet_filename:2}
